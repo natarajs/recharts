0e9cc32d0d9c4ca493a59cd77ec100e2f80cebc8
natarajs <lakshmi_sayee@yahoo.com>
2016-04-26 14:40:50 -0400

    Updated to new JS binding

--- inst/htmlwidgets/echarts.js
+++ inst/htmlwidgets/echarts.js
@@ -2,15 +2,29 @@ HTMLWidgets.widget({
   name: 'echarts',
   type: 'output',
 
-  initialize: function(el, width, height) {
-    return echarts.init(el);
-  },
+  factory: function(el, width, height) {
+    // create our sigma object and bind it to the element
+    var echart = new echarts.init(el);
+    return {
+      renderValue: function(x) {
+        echart.setOption(x);
 
-  renderValue: function(el, x, instance) {
-    instance.setOption(x);
-  },
-
-  resize: function(el, width, height, instance) {
+        // parse gexf data
+        //var parser = new DOMParser();
+        //var data = parser.parseFromString(x.data, "application/xml");
+        // apply settings
+        //for (var name in x.settings)
+          //sig.settings(name, x.settings[name]);
+      },
+      
+      resize: function(width, height) {
+      },
+      
+      // Make the sigma object available as a property on the widget
+      // instance we're returning from factory(). This is generally a
+      // good idea for extensibility--it helps users of this widget
+      // interact directly with sigma, if needed.
+      e: echart
+    };
   }
-
 });
